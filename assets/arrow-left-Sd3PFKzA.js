db0c7918b8aa784be0b68021fcb778f3b1be6e20
Yogesh-VG0 <yogesh.vg456@gmail.com>
2026-03-04 20:35:24 +0400

    Updates

--- assets/arrow-left-Sd3PFKzA.js
+++ assets/arrow-left-Sd3PFKzA.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-CyfTwbzm.js";/**
+import{c as e}from"./index-BzKHJunE.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
