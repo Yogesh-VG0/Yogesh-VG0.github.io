539f565bdaaf047f6709cb215af9c947031f982d
Yogesh-VG0 <yogesh.vg456@gmail.com>
2026-04-17 05:52:13 +0400

    Deploy: update ExpenseVision project details

--- assets/arrow-left-B2IkIaRf.js
+++ assets/arrow-left-B2IkIaRf.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-BtmuVgS5.js";/**
+import{c as e}from"./index-cdIFJdeB.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
